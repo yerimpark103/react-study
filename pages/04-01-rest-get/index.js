7ad33d6094b4037381239c1e273e60e549b04d12
claud_park <claudia@relate.so>
2023-03-09 12:02:37 +0900

    Add 04-01

--- pages/04-01-rest-get/index.js
+++ pages/04-01-rest-get/index.js
@@ -1,7 +1,10 @@
 import {Button} from 'antd';
 import axios from 'axios';
+import {useState} from 'react';
 
 export default function RestGetPage() {
+  const [title, setTitle] = useState('');
+
   function onClickAsync() {
     const result = axios.get('https://koreanjson.com/posts/1');
     console.log(result);
@@ -11,6 +14,7 @@ export default function RestGetPage() {
   async function onClickSync() {
     const result = await axios.get('https://koreanjson.com/posts/1');
     console.log(result);
+    setTitle(result.data.title);
     // {data: {…}, status: 200, statusText: 'OK', headers: AxiosHeaders, config: {…}, …}
   }
 
@@ -18,6 +22,7 @@ export default function RestGetPage() {
     <>
       <Button onClick={onClickAsync}>REST-API 비동기 요청하기</Button>
       <Button onClick={onClickSync}>REST-API 동기 요청하기</Button>
+      <h1>{title}</h1>
     </>
   );
 }
